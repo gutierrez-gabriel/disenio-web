6780809e2751a33634ae9a6202f8e810b676ea51
Martuleva1 <128637500+Martuleva1@users.noreply.github.com>
2023-04-28 01:39:45 -0300

    Borro js

--- js/scrip.js
+++
@@ -1,7 +0,0 @@
-const Acordeon = document.getElementsByClassName('UNO');
-
-for (i=0; i<Acordeon.length; i++) {
-  Acordeon[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-  })
-}
